6a0126a329e01ddafbfde4244b587e2026a2d470
RafaelTS <rafaeltorress@gmail.com>
2023-06-23 20:32:08 -0300

    basic structure

---
+++ src/main/java/br/sc/rafael/services/LocacaoService.java
@@ -0,0 +1,32 @@
+package br.sc.rafael.services;
+
+import br.sc.rafael.entities.Filme;
+import br.sc.rafael.entities.Locacao;
+import br.sc.rafael.entities.Usuario;
+
+import java.util.Date;
+
+public class LocacaoService {
+
+    public Locacao alugarFilme(Usuario usuario, Filme filme) {
+        Locacao locacao = new Locacao();
+        locacao.setFilme(filme);
+        locacao.setUsuario(usuario);
+        locacao.setDataLocacao(new Date());
+        locacao.setValor(filme.getPrecoLocacao());
+
+        //Entrega no dia seguinte
+        Date dataEntrega = new Date();
+        dataEntrega = adicionarDias(dataEntrega, 1);
+        locacao.setDataRetorno(dataEntrega);
+
+        //Salvando a locacao...
+        //TODO adicionar método para salvar
+
+        return locacao;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
