a0d70231fb19336b958c8f98d7de6a7465b8ee25
RafaelTS <rafaeltorress@gmail.com>
2023-07-09 17:49:23 -0300

    Simple TDD example

---
+++ src/main/java/br/sc/rafael/exceptions/NaoPodeDividirPorZeroException.java
@@ -0,0 +1,6 @@
+package br.sc.rafael.exceptions;
+
+public class NaoPodeDividirPorZeroException extends Exception{
+    private static final long serialVersionUID = 1L;
+
+}
---
+++ src/test/java/br/sc/rafael/services/CalculadoraTest.java
@@ -0,0 +1,55 @@
+package br.sc.rafael.services;
+
+import br.sc.rafael.exceptions.NaoPodeDividirPorZeroException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculadoraTest {
+
+    private Calculadora calc;
+
+    @Before
+    public void setup() {
+        calc = new Calculadora();
+    }
+
+    @Test
+    public void deveSomarDoisValores() {
+
+        int a= 5;
+        int b= 3;
+
+        int resultado = calc.somar(a, b);
+
+        Assert.assertEquals(8, resultado);
+    }
+
+    @Test
+    public void deveSubtrairDoisValores() {
+        int a = 8;
+        int b = 5;
+
+        int resultado = calc.subtrair(a, b);
+
+        Assert.assertEquals(3, resultado);
+    }
+
+    @Test
+    public void deveDividirDoisValores() throws NaoPodeDividirPorZeroException {
+        int a = 6;
+        int b = 3;
+
+        int resultado = calc.divide(a, b);
+
+        Assert.assertEquals(2, resultado);
+    }
+
+    @Test(expected =  NaoPodeDividirPorZeroException.class)
+    public void deveLancarExcecaoAoDividirPorZero() throws NaoPodeDividirPorZeroException {
+        int a = 10;
+        int b = 0;
+
+        calc.divide(a, b);
+    }
+}
