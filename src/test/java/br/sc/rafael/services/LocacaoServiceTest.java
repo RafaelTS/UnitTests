d072b36c8ad2a98ba201bdbc7ecaf86993eec1a0
RafaelTS <rafaeltorress@gmail.com>
2023-07-09 00:26:59 -0300

    adding List of movies

--- src/test/java/br/sc/rafael/services/LocacaoServiceTest.java
+++ src/test/java/br/sc/rafael/services/LocacaoServiceTest.java
@@ -9,7 +9,10 @@ import org.junit.*;
 import org.junit.rules.ErrorCollector;
 import org.junit.rules.ExpectedException;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 import static br.sc.rafael.Utils.DataUtils.*;
 import static org.hamcrest.CoreMatchers.*;
@@ -39,10 +42,10 @@ public class LocacaoServiceTest {
 
         //cenario
         Usuario usuario = new Usuario("Usuario 1");
-        Filme filme =  new Filme("Filme 1", 2, 5.0);
+        List <Filme> filmes = Arrays.asList(new Filme("Filme 1", 2, 5.0));
 
         //acao
-        Locacao locacao = service.alugarFilme(usuario, filme);
+        Locacao locacao = service.alugarFilme(usuario, filmes);
 
             //verificacao
             error.checkThat(locacao.getValor(), is(equalTo(5.0)));
@@ -57,18 +60,18 @@ public class LocacaoServiceTest {
 
         //cenario
         Usuario usuario = new Usuario("Usuario 1");
-        Filme filme = new Filme("Filme 1", 0, 5.0);
+        List <Filme> filmes = Arrays.asList(new Filme("Filme 1", 0, 4.0));
 
-        service.alugarFilme(usuario, filme);
+        service.alugarFilme(usuario, filmes);
     }
 
     @Test
     public void testLocacao_usuarioVazio() throws FilmeSemEstoqueException {
 
-        Filme filme = new Filme("Filme 1", 1, 5.0);
+        List <Filme> filmes = Arrays.asList(new Filme("Filme 1", 2, 4.0));
 
         try {
-            service.alugarFilme(null, filme);
+            service.alugarFilme(null, filmes);
             fail();
         } catch (LocadoraException e) {
             assertThat(e.getMessage(), is("Usuario vazio"));
