f0f4919abce17f0c28e0741e9b7178aac322c152
RafaelTS <rafaeltorress@gmail.com>
2023-09-24 19:05:15 -0300

    spy

--- src/test/java/br/sc/rafael/services/CalculadoraMockTest.java
+++ src/test/java/br/sc/rafael/services/CalculadoraMockTest.java
@@ -2,12 +2,31 @@ package br.sc.rafael.services;
 
 import br.sc.rafael.entities.Locacao;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.*;
 
 public class CalculadoraMockTest {
+
+    @Mock
+    private Calculadora calcMock;
+
+    @Spy
+    private Calculadora calcSpy;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void devoMostrarDiferencaEntreMockSpy() {
+        Mockito.when(calcMock.somar(1, 2)).thenReturn(6);
+        Mockito.when(calcSpy.somar(1, 2)).thenReturn(8);
+
+        System.out.println("Mock: " + calcMock.somar(1, 2));
+        System.out.println("Spy: " + calcSpy.somar(1, 2));
+    }
 
     @Test
     public void teste() {
